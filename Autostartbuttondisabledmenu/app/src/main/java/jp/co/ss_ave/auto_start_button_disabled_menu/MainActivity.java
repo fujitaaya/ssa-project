aa0a7ce3fe90e14974a8a8ca2bd9f16e7f6a93b8
fujitaaya <a-fujita@ss-ave.co.jp>
2018-02-27 13:21:20 +0900

    base commit

---
+++ Autostartbuttondisabledmenu/app/src/main/java/jp/co/ss_ave/auto_start_button_disabled_menu/MainActivity.java
@@ -0,0 +1,118 @@
+package jp.co.ss_ave.auto_start_button_disabled_menu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        //buttonを取得
+        findViewById(R.id.Button01).setOnClickListener(this);
+        findViewById(R.id.Button02).setOnClickListener(this);
+        findViewById(R.id.Button03).setOnClickListener(this);
+
+
+//        //クラス名取得
+//        Intent it = new Intent(Intent.ACTION_MAIN, null);
+//        it.addCategory(Intent.CATEGORY_LAUNCHER);
+//        PackageManager pManager = getPackageManager();
+//        List<ResolveInfo> appList =  pManager.queryIntentActivities(it,0);
+//        Collections.sort(appList,new ResolveInfo.DisplayNameComparator(pManager));
+//        //Dataにして
+//        String textdata = "";
+//        for (ResolveInfo info : appList) {
+//            textdata = textdata + info.activityInfo.name +"\n";
+//        }
+//        //暗黙のintentでデータを投げる
+//        Intent intent = new Intent();
+//        intent.setAction(Intent.ACTION_SEND);
+//        intent.putExtra(Intent.EXTRA_TEXT, textdata);
+//        intent.setType( "text/plain" );
+//        startActivity(intent);
+//
+//        Log.d("_test_",textdata);
+    }
+
+    public void onClick(View v) {
+        PackageManager pm = getPackageManager();
+        Intent intent = new Intent();
+
+        // 各アプリ起動
+        if(v != null){
+            switch(v.getId()){
+                case R.id.Button01:
+                    intent = pm.getLaunchIntentForPackage("com.android.calculator2");
+                    break;
+
+                case R.id.Button02:
+                    intent = pm.getLaunchIntentForPackage("com.android.calendar");
+                    break;
+
+                case R.id.Button03:
+                    intent = pm.getLaunchIntentForPackage("com.android.settings");
+                    break;
+            }
+        }
+
+        try{
+            startActivity(intent);
+        }catch(Exception e){
+            Toast.makeText(MainActivity.this, "対象のアプリがありません", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // ホームアプリのため、戻るボタン無効
+    }
+
+}
