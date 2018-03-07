24e1540ba6c5b685b43066616e060aff2573f1d3
fujitaaya <a-fujita@ss-ave.co.jp>
2018-03-07 14:48:48 +0900

    commit

--- Autostartbuttondisabledmenu/app/src/main/java/jp/co/ss_ave/auto_start_button_disabled_menu/MainActivity.java
+++ Autostartbuttondisabledmenu/app/src/main/java/jp/co/ss_ave/auto_start_button_disabled_menu/MainActivity.java
@@ -1,17 +1,33 @@
 package jp.co.ss_ave.auto_start_button_disabled_menu;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -35,7 +51,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.Button01).setOnClickListener(this);
         findViewById(R.id.Button02).setOnClickListener(this);
         findViewById(R.id.Button03).setOnClickListener(this);
-
+        findViewById(R.id.Button04).setOnClickListener(this);
+        findViewById(R.id.Button05).setOnClickListener(this);
 
 //        //クラス名取得
 //        Intent it = new Intent(Intent.ACTION_MAIN, null);
@@ -56,7 +73,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
 //        startActivity(intent);
 //
 //        Log.d("_test_",textdata);
+
     }
+
+
 
     public void onClick(View v) {
         PackageManager pm = getPackageManager();
@@ -75,6 +95,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
                 case R.id.Button03:
                     intent = pm.getLaunchIntentForPackage("com.android.settings");
+                    break;
+
+                case R.id.Button04:
+                    intent = new Intent(getApplication(), jp.co.ss_ave.auto_start_button_disabled_menu.ListActivity.class);
+                    break;
+
+                case R.id.Button05:
+
+
                     break;
             }
         }
@@ -114,5 +143,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onBackPressed() {
         // ホームアプリのため、戻るボタン無効
     }
+
+    protected void onActivityResult( int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(resultCode == RESULT_OK && null != intent) {
+            String res = intent.getStringExtra("appName");
+            Toast.makeText(MainActivity.this, res, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 
 }
