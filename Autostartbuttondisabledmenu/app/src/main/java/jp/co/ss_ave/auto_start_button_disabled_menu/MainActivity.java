25f81548952955ec10782a7ede63229175193a70
fujitaaya <a-fujita@ss-ave.co.jp>
2018-03-08 18:09:53 +0900

    ショートカット作成

--- Autostartbuttondisabledmenu/app/src/main/java/jp/co/ss_ave/auto_start_button_disabled_menu/MainActivity.java
+++ Autostartbuttondisabledmenu/app/src/main/java/jp/co/ss_ave/auto_start_button_disabled_menu/MainActivity.java
@@ -2,14 +2,19 @@ package jp.co.ss_ave.auto_start_button_disabled_menu;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -23,13 +28,27 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    final int button01 = 0;
+    final int button02 = 1;
+    final int button03 = 2;
+    final int button04 = 3;
+    final int iniCheck = 999;
+
+    public Intent shortcutIntent = null;
+
+
+    int clickCheck = iniCheck;
+    boolean[] buttonFlag = new boolean[4];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +67,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
 //        });
 
         //buttonを取得
+//        ImageButton button = (ImageButton) findViewById(R.id.Button01);
         findViewById(R.id.Button01).setOnClickListener(this);
         findViewById(R.id.Button02).setOnClickListener(this);
         findViewById(R.id.Button03).setOnClickListener(this);
         findViewById(R.id.Button04).setOnClickListener(this);
-        findViewById(R.id.Button05).setOnClickListener(this);
+//        findViewById(R.id.Button05).setOnClickListener(this);
+
+
 
 //        //クラス名取得
 //        Intent it = new Intent(Intent.ACTION_MAIN, null);
@@ -86,24 +108,47 @@ public class MainActivity extends Activity implements View.OnClickListener {
         if(v != null){
             switch(v.getId()){
                 case R.id.Button01:
-                    intent = pm.getLaunchIntentForPackage("com.android.calculator2");
+                    if(buttonFlag[button01] == false){
+                        intent = new Intent(getApplication(), jp.co.ss_ave.auto_start_button_disabled_menu.ListActivity.class);
+                        buttonCheck(button01);
+                        clickCheck = button01;
+                        startActivityForResult(intent, 1);
+                    }else{
+                        intent = shortcutIntent;
+                    }
                     break;
 
                 case R.id.Button02:
-                    intent = pm.getLaunchIntentForPackage("com.android.calendar");
+                    if(buttonFlag[button02] == false){
+                        intent = new Intent(getApplication(), jp.co.ss_ave.auto_start_button_disabled_menu.ListActivity.class);
+                        buttonCheck(button02);
+                        clickCheck = button02;
+                        startActivityForResult(intent, 1);
+                    }else{
+                        intent = shortcutIntent;
+                    }
                     break;
 
                 case R.id.Button03:
-                    intent = pm.getLaunchIntentForPackage("com.android.settings");
+                    if(buttonFlag[button03] == false){
+                        intent = new Intent(getApplication(), jp.co.ss_ave.auto_start_button_disabled_menu.ListActivity.class);
+                        buttonCheck(button03);
+                        clickCheck = button03;
+                        startActivityForResult(intent, 1);
+                    }else{
+                        intent = shortcutIntent;
+                    }
                     break;
 
                 case R.id.Button04:
-                    intent = new Intent(getApplication(), jp.co.ss_ave.auto_start_button_disabled_menu.ListActivity.class);
-                    break;
-
-                case R.id.Button05:
-
-
+                    if(buttonFlag[button04] == false){
+                        intent = new Intent(getApplication(), jp.co.ss_ave.auto_start_button_disabled_menu.ListActivity.class);
+                        buttonCheck(button04);
+                        clickCheck = button04;
+                        startActivityForResult(intent, 1);
+                    }else{
+                        intent = shortcutIntent;
+                    }
                     break;
             }
         }
@@ -147,12 +192,60 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onActivityResult( int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
+        PackageManager pm = getPackageManager();
+
         if(resultCode == RESULT_OK && null != intent) {
-            String res = intent.getStringExtra("appName");
-            Toast.makeText(MainActivity.this, res, Toast.LENGTH_SHORT).show();
+
+            Bitmap icon =  (Bitmap) intent.getExtras().get("icon");
+
+            if(clickCheck != iniCheck){
+                switch (clickCheck){
+                    case button01:
+                        ((ImageButton)findViewById(R.id.Button01)).setImageBitmap(icon);
+                        ((ImageButton)findViewById(R.id.Button01)).setBackground(null);
+                        break;
+
+                    case button02:
+                        ((ImageButton)findViewById(R.id.Button02)).setImageBitmap(icon);
+                        ((ImageButton)findViewById(R.id.Button02)).setBackground(null);
+                        break;
+
+                    case button03:
+                        ((ImageButton)findViewById(R.id.Button03)).setImageBitmap(icon);
+                        ((ImageButton)findViewById(R.id.Button03)).setBackground(null);
+                        break;
+
+                    case button04:
+                        ((ImageButton)findViewById(R.id.Button04)).setImageBitmap(icon);
+                        ((ImageButton)findViewById(R.id.Button04)).setBackground(null);
+                        break;
+                }
+            }
+
+            shortcutIntent = pm.getLaunchIntentForPackage(intent.getStringExtra("packageName"));
+            clickCheck = iniCheck;
         }
     }
 
+    protected void buttonCheck(int Flag){
 
+        switch (Flag){
+            case button01:
+                buttonFlag[button01] = true;
+                break;
+
+            case button02:
+                buttonFlag[button02] = true;
+                break;
+
+            case button03:
+                buttonFlag[button03] = true;
+                break;
+
+            case button04:
+                buttonFlag[button04] = true;
+                break;
+        }
+    }
 
 }
